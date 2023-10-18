61b21ef79978f9d81577defe11ecc12fdc19e6b5
Tanjil Hossain <tanjilhossain845@gmail.com>
2023-10-18 10:01:06 +0600

    compleate brand api

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 require('dotenv').config();
 const app = express();
@@ -30,12 +30,38 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+        const brandCollection = client.db("brandDb").collection('brands');
+
+        //brand api
+
+        app.get("/brands", async (req, res) => {
+            const result = await brandCollection.find().toArray();
+            res.send(result);
+        });
+
+        app.get("/brands/:id", async (req, res) => {
+            const brandId = req.params.id;
+            const filter = { _id: new ObjectId(brandId) };
+            const result = await brandCollection.findOne(filter);
+            res.send(result);
+        });
+
+        app.post('/brands', async (req, res) => {
+            const brandInfo = req.body;
+            const doc = {
+                brandName: brandInfo?.brandName,
+                image: brandInfo?.image
+            };
+            const result = await brandCollection.insertOne(doc);
+            res.send(result);
+        });
+        ////////brand api end
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
-        await client.close();
+        // await client.close();
     }
 }
 run().catch(console.dir);
