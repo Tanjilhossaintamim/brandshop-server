67567ee06e20a87ad9a06036185de5d9e161592d
Tanjil Hossain <tanjilhossain845@gmail.com>
2023-10-18 12:23:42 +0600

    compleate all api operations

--- index.js
+++ index.js
@@ -30,7 +30,10 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
-        const brandCollection = client.db("brandDb").collection('brands');
+        const brandDb = client.db("brandDb");
+        const brandCollection = brandDb.collection('brands');
+        const productsCollection = brandDb.collection("products");
+        const cartCollection = brandDb.collection('carts');
 
         //brand api
 
@@ -56,6 +59,94 @@ async function run() {
             res.send(result);
         });
         ////////brand api end
+
+        // products api
+        app.get('/products/:brandName', async (req, res) => {
+            const brandName = req.params.brandName;
+            const filter = { brandName };
+            const result = await productsCollection.find(filter).toArray();
+            res.send(result);
+        });
+        app.get('/products/:brandName/:id', async (req, res) => {
+            const productId = req.params.id;
+            const filter = {
+                _id: new ObjectId(productId)
+            };
+            const result = await productsCollection.findOne(filter);
+
+            res.send(result);
+        });
+        app.post('/products', async (req, res) => {
+            const productInfo = req.body;
+            const doc = {
+                image: productInfo?.image,
+                name: productInfo?.name,
+                brandName: productInfo?.brandName,
+                type: productInfo?.type,
+                price: productInfo?.price,
+                description: productInfo?.description,
+                rating: productInfo?.rating
+            };
+            const result = await productsCollection.insertOne(doc);
+            res.send(result);
+
+        });
+        app.put('/products/:brandName/:id', async (req, res) => {
+            const productId = req.params.id;
+            const updatedProductInfo = req.body;
+            const filter = {
+                _id: new ObjectId(productId)
+            };
+            const options = { upsert: true };
+            const updatedDoc = {
+                $set: {
+                    image: updatedProductInfo?.image,
+                    name: updatedProductInfo?.name,
+                    brandName: updatedProductInfo?.brandName,
+                    type: updatedProductInfo?.type,
+                    price: updatedProductInfo?.price,
+                    description: updatedProductInfo?.description,
+                    rating: updatedProductInfo?.rating
+                }
+            };
+            const result = await productsCollection.updateOne(filter, updatedDoc, options);
+            res.send(result);
+        });
+        // product api end
+        // shoping cart api
+        app.get('/carts/:userEmail', async (req, res) => {
+            const userEmail = req.params.userEmail;
+            const filter = {
+                email: userEmail
+            };
+            const result = await cartCollection.find(filter).toArray();
+            res.send(result);
+        });
+        app.post('/carts/:id', async (req, res) => {
+            const info = req.body;
+            const id = req.params.id;
+            info.id = id;
+            const userEmail = req.body.email;
+            const filter = {
+                email: userEmail,
+                id: req.body.id
+            };
+            const isAlreadyExists = await cartCollection.findOne(filter);
+            if (isAlreadyExists?._id) {
+                res.send({ error: "Product Allready exists in your cart !" });
+            } else {
+
+                const result = await cartCollection.insertOne(info);
+                res.send(result);
+            }
+
+        });
+        app.delete('/carts/:id', async (req, res) => {
+            const productId = req.params.id;
+            const query = { _id: new ObjectId(productId) };
+            const result = await cartCollection.deleteOne(query);
+            res.send(result);
+        });
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
